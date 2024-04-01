821f498992e881d853290b7cd91c3d9931edd8f2
SkantronZero <150893003+SkantronZero@users.noreply.github.com>
2024-03-31 22:47:22 -0600

    1st working submission

--- script.js
+++ script.js
@@ -2,49 +2,54 @@ const function_test_text = document.querySelector("#function_test_text");
 const computer_choice_text = document.querySelector("#computer_choice_text");
 const player_RPS_choice = document.querySelector("#player_RPS_choice");
 const RPS_result_text = document.querySelector("#RPS_result_text");
-// const player_RPS_button = document.querySelector("#player_RPS_button");
 const RPS = ['Rock','Paper','Scissors'];
-
-// player_RPS_choice.textContent = "Rock";
-
-function_test_text.textContent = 'Hello World!';
-
-RPS_result_text.textContent = 'Testing';
-
-// console.log('player_RPS_choice type = ' + typeof(player_RPS_choice.textContent));
-// console.log('player_RPS_choice = ' + player_RPS_choice.textContent);
-
-
 
 function computerChoice(){
     let chosenNum = (Math.floor(Math.random()*3));
-    // console.log(chosenNum);
     return chosenNum;
 }
 
 
 function getComputerChoice(){
     let chosenRPS = RPS[computerChoice()];
-    // console.log(chosenRPS);
     return chosenRPS;
 }
 
-// function playRPS(){
-//     console.log(player_RPS_choice.textContent)
-//     let computer_RPS_choice = getComputerChoice();
+function playRPS(){
+    console.log(player_RPS_choice.value);
+    if (player_RPS_choice.value == ''){
+        alert('Please choose Rock, Paper, or Scissors.');
+        return;
+    }
 
-//     computer_RPS_choice = 'Rock';
+    let player_RPS_choice_index = RPS.indexOf(player_RPS_choice.value);
+    console.log('index of player choice: ' + player_RPS_choice_index);
+
+    let winner = '';
+
+    let computer_RPS_choice = getComputerChoice();
+    let computer_RPS_choice_index = RPS.indexOf(computer_RPS_choice);
+    console.log('index of computer choice: '+ computer_RPS_choice_index);
+
+    if (player_RPS_choice_index == computer_RPS_choice_index){
+        RPS_result_text.textContent = "Draw";
+        winner = 'Draw';
+    }
+    else if ((computer_RPS_choice_index - player_RPS_choice_index == 1) || (player_RPS_choice_index - computer_RPS_choice_index == 2) ){
+        RPS_result_text.textContent = `${computer_RPS_choice} beats ${player_RPS_choice.value}. Computer wins.`;
+        winner = "Computer";
+    }
+    else {
+        RPS_result_text.textContent = `${player_RPS_choice.value} beats ${computer_RPS_choice}. Player wins.`;
+        winner = "Player";
+    }
     
-//     switch(player_RPS_choice == 'Rock'){
-        
-//         case 0:
-//             alert(computer_RPS_choice);
-//             if (computer_RPS_choice == 'Rock'){
-//                 RPS_result_text.textContent = 'Draw';
-                
-//             }
-//     }
-// }
+    console.log('The winner is : '+winner);
+
+    computer_RPS_choice = '';
+    return;
+
+}
 
 function update_result(){
     RPS_result_text.textContent = 'Clicked';
@@ -52,6 +57,5 @@ function update_result(){
 
 console.log(getComputerChoice());
 
-computer_choice_text.textContent = getComputerChoice();
 
-player_RPS_button.addEventListener("click", update_result);
+player_RPS_button.addEventListener("click", playRPS);
